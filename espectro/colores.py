fa52827958fa22163de5b8571e154a9c72e8e15d
Ale-Mildiner <alemildiner26@gmail.com>
2021-11-05 16:53:44 -0300

    actualizo algo de colores, nose si esta muy bien igual

--- espectro/colores.py
+++ espectro/colores.py
@@ -42,3 +42,30 @@ colors = ['red', 'green', 'blue']
 for i in range(len(RGB)):
     plt.plot(lon_RGB[i], int_RGB[i], color= colors[i],label = RGB[i])
 plt.show()
+
+#%%
+import os
+
+from colour.plotting import *
+import colour as colour
+colour_style();
+
+#%%
+inte = []
+long_onda = []
+inte_lamp = []
+lon = np.arange(193, 1200)
+for j in lon:
+    for i in range(len(lon_papel[0])-1):
+        if lon_papel[0][i+1]>j and lon_papel[0][i]<j:
+            #print(lon_papel[0][i])
+            long_onda.append(int(lon_papel[0][i]))
+            inte.append(int_papel[4][i])
+            inte_lamp.append(int_lampara[i])
+#%%
+data_dic = dict(zip(long_onda,np.array(inte)/np.array(inte_lamp)))
+sdm     = ( colour.SpectralDistribution(data_dic)).normalise()
+XYZ = colour.sd_to_XYZ(sdm)
+RGB = colour.XYZ_to_sRGB(XYZ)
+plt.style.use({'figure.figsize': (5, 5)})
+plot_single_colour_swatch(ColourSwatch('Mediodía', RGB/np.max(RGB)),text_parameters={'size': 'x-large'});            
